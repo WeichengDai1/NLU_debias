e516a128e5b61ad868227c5b39214a2dc7079be6
WeichengDai1 <daiweicheng4536@gmail.com>
2023-05-11 15:54:07 -0400

    add dim

---
+++ train.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python code/_main_NER_EEC.py --hidden_layer 55 
+CUDA_VISIBLE_DEVICES=0 python code/_main_NER_EEC.py --hidden_layer 60
+
+CUDA_VISIBLE_DEVICES=0 python code/_main_NER_EEC.py --hidden_layer 65 
+CUDA_VISIBLE_DEVICES=0 python code/_main_NER_EEC.py --hidden_layer 70
